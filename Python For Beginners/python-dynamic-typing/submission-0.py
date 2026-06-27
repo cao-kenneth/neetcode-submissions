2983b5de12c7fa988181e85567620a5ff04c45d5
Kenneth Cao <kc.kennethcao@gmail.com>
2026-06-26 23:27:49 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "hi"
+print(type(variable))
+variable = [1]
+print(type(variable))
